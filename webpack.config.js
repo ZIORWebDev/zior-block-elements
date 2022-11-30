7a4a61e9488c42f218e63b93c494cbd77db3be08
Rey Calantaol <reygcalantaol@gmail.com>
2022-12-01 01:42:24 +0800

    Webpack Config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+/**
+ * External Dependencies
+ */
+const path = require("path");
+
+/**
+* WordPress Dependencies
+*/
+const defaultConfig = require("@wordpress/scripts/config/webpack.config.js");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+    ...defaultConfig,
+    entry: {
+        index: path.resolve(__dirname, "src", "index.js"),
+    },
+    output: {
+        path: path.resolve(__dirname, "build/"),
+        filename: "zr-blocks.min.js"
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'zr-blocks.min.css',
+        }),
+    ]
+}
